493a1029fc079ece65ce5c9c94b15122929aa15e
Amaan Kulshreshtha <amaan.kulshreshtha@gmail.com>
2019-02-26 00:22:25 +0400

    add comments for clarification in HOC

--- higher-order-components/src/App.js
+++ higher-order-components/src/App.js
@@ -1,6 +1,38 @@
 import React from 'react'
 import './App.css'
 
+/* 
+  This is an example of higher order components
+
+  It is inspired by higher order functions: functions that take other functions as arguments or 
+  their value is a function
+
+  Ex: 
+
+  const add = (a, b) => a + b;
+
+  console.log(add(9, 8)) 
+  // Output: 17
+
+  // Now suppose we want to make an adder that utilises the add function
+
+  const adder = (a) => (b) => add(a, b)
+  
+  // For ease of understanding, we can write the above like this as well
+  const adder = function(a) {
+    return function(b) {
+      add(a, b);
+    };
+  }
+
+  const add2 = adder(5);
+
+  console.log(add2(8))
+  // Output: 13
+*/
+
+
+// In this example, we are taking the Component as the input and sending it rendering it as Another Component
 const withMouse = (Component) => {
   return class extends React.Component {
     state = { x: 0, y: 0 };
@@ -12,6 +44,7 @@ const withMouse = (Component) => {
       });
     };
 
+    // returning passed component as another component
     render() {
       return (
         <div style={{ height: "100%" }} onMouseMove={this.handleMouseMove}>
